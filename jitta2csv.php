3e68554cce59edb4254f09c3bb90b228dccecd99
Chua Wei Jie <chuawei_jie@hotmail.com>
2019-02-19 11:22:43 +0700

    Added Price Extraction
    1. Wrote the regular expression for price extraction
    2. Added price into the export logic.

--- jitta2csv.php
+++ jitta2csv.php
@@ -8,6 +8,7 @@
 	preg_match_all('/<div class="Text-gbmmix-1 (ciqyXm|evKeBE)" color="#\w{6}">(.+)<\/div>/U', $jitta_html, $arr_percentage_to_line);
 	preg_match_all('/<div class="Text__TextXS-gbmmix-3 \w{5}" color="#\w{6}">(Below|Above) Jitta Line<\/div>/U', $jitta_html, $arr_above_below_line);
 	preg_match_all('/<div class="Text-gbmmix-1 fDxapP" color="#6c7172">(\d+.\d%)<\/div>/U', $jitta_html, $arr_loss_chance);
+	preg_match_all('/<div class="Text-gbmmix-1 fDxapP" color="#6c7172">฿(.+)<\/div>/U', $jitta_html, $arr_price);
 	
 	//Get the length of all the arrays
 	$ticker_len = count($arr_ticker[1]);
@@ -15,13 +16,13 @@
 	$percentage_to_line_len = count($arr_percentage_to_line[2]);
 	$above_or_below_len = count($arr_above_below_line[1]);
 	$arr_loss_chance_len = count($arr_loss_chance[1]);
-	
+	$arr_price_len = count($arr_price[1]);
 	
 	//Out put it to csv if all the arrays are the same. 
-	if( $ticker_len == $jitta_score_len && $jitta_score_len == $percentage_to_line_len && $percentage_to_line_len == $above_or_below_len && $above_or_below_len == $arr_loss_chance_len){
+	if( $ticker_len == $jitta_score_len && $jitta_score_len == $percentage_to_line_len && $percentage_to_line_len == $above_or_below_len && $above_or_below_len == $arr_loss_chance_len && $arr_loss_chance_len==$arr_price_len){
 		$myfile = fopen("output.csv", "w") or die("Unable to open file!");
 		for($i=0;$i<$ticker_len;$i++){
-			fwrite($myfile, $arr_ticker[1][$i].','.$arr_jitta_score[1][$i].','.$arr_percentage_to_line[2][$i].','.$arr_above_below_line[1][$i].','.$arr_loss_chance[1][$i]."\n");
+			fwrite($myfile, $arr_ticker[1][$i].','.$arr_jitta_score[1][$i].','.$arr_percentage_to_line[2][$i].','.$arr_above_below_line[1][$i].','.$arr_loss_chance[1][$i].','.$arr_price[1][$i]."\n");
 		}
 		
 		fclose($myfile);
